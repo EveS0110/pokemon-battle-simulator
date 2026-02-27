e6bd32fa17182d3b97b5b8c0e06758ae441e0039
EveS0110 <evellysena550@gmail.com>
2026-02-27 19:50:04 -0300

    Ajustes no addPokes.js antes de mudar de branch

--- assets/js/addPokes.js
+++ assets/js/addPokes.js
@@ -1,69 +1,75 @@
 import getPokemon from "./getPokemon.js";
-import { introMusic, playWithLimit } from "./index.js";
+import { introMusic, playChoose, playHover} from "./sounds.js";
+import { setPlayerPokemons } from "./playersData.js";
 
-function addPoke(card) {
-    let pokeCard = document.querySelectorAll(".poke-option");
-    const pokeMain = document.getElementById(`${card}`);
 
-    pokeCard.forEach(el => {
-        let pokeClicked
-        const chosenPoke = new Audio("assets/audio/WhatsApp Audio 2026-02-23 at 20.51.48.mpeg");
+function addPoke(card1, card2, jogador) {
+  let slotAtual = 0;
+  const pokemonsEscolhidos = [];
 
-        const hoverCard = new Audio("assets/audio/hoverCard.mpeg");
+  const cards = [
+    document.getElementById(card1),
+    document.getElementById(card2)
+  ];
 
-        el.addEventListener("mouseenter", async () => {
-            hoverCard.currentTime = 0;
-            hoverCard.play();
+  const modal = document.querySelector(".modal-poke");
+  if (!modal) return;
 
-            setTimeout(() => {
-                hoverCard.pause();
-                hoverCard.currentTime = 0;
-            }, 2000);
-        });
+  modal.addEventListener("mouseover", (e) => {
+  const option = e.target.closest(".poke-option");
+  if (!option) return;
 
-        el.addEventListener("click", async () => {
-            pokeClicked = el.lastElementChild.textContent
+  playHover(); 
+});
 
-            const pokemon = await getPokemon(pokeClicked);
-            console.log(pokemon)
-            const modal = document.querySelector(".modal-poke");
+modal.addEventListener("click", (e) => {
+  const option = e.target.closest(".poke-option");
+  if (!option) return;
 
-            pokeMain.innerHTML = `
-            <img class="poke-img" src="${pokemon.image}" alt="${pokemon.name}">
+  playChoose(); 
 
-            <div class="poke-infor">
-                <h3 class="poke-name">${pokemon.name}</h3>
+  escolherPokemon(option);
+});
 
-                <span class="type type-${pokemon.types[0]}">${[...pokemon.types]}</span>
+async function escolherPokemon(option) {
+  if (slotAtual >= cards.length) return;
 
-                <div class="atributes">
-                <div class="hp">
-                    <h3>HP</h3>
-                    <p>${pokemon.hp}</p>
-                </div>
+  const name = option.dataset.name;
+  const pokemon = await getPokemon(name);
 
-                <div class="ataque">
-                    <h3>Ataque</h3>
-                    <p>${pokemon.attack}</p>
-                </div>
+    const target = cards[slotAtual];
 
-                <div class="defesa">
-                    <h3>Defesa</h3>
-                    <p>${pokemon.defense}</p>
-                </div>
-            </div>
-            `
-            chosenPoke.play();
+    target.innerHTML = `
+      <img class="poke-img" src="${pokemon.image}">
+      <div class="poke-infor">
+        <h3 class="poke-name">${pokemon.name}</h3>
+        <span class="type type-${pokemon.types[0]}">
+          ${pokemon.types.join(", ")}
+        </span>
+        <div class="atributes">
+          <div class="hp"><h3>HP</h3><p>${pokemon.hp}</p></div>
+          <div class="ataque"><h3>Ataque</h3><p>${pokemon.attack}</p></div>
+          <div class="defesa"><h3>Defesa</h3><p>${pokemon.defense}</p></div>
+        </div>
+      </div>
+    `;
+    pokemonsEscolhidos.push(pokemon);
+    slotAtual++;
 
-            setTimeout(() => {
-                chosenPoke.pause();
-                chosenPoke.currentTime = 0;
-            }, 2000);
+    if (slotAtual === cards.length) {
+      const pokemonsEscolhidos = cards.map(card => ({
+        name: card.querySelector(".poke-name")?.textContent,
+        hp: Number(card.querySelector(".hp p")?.textContent),
+        attack: Number(card.querySelector(".ataque p")?.textContent),
+        defense: Number(card.querySelector(".defesa p")?.textContent),
+        image: card.querySelector(".poke-img")?.src
+      }));
 
-            introMusic.play();
-            return modal.remove();
-        });
-    });
-};
+      const jogador = card1.includes("One") ? 1 : 2;
+      setPlayerPokemons(jogador, pokemonsEscolhidos);
+
+      modal.remove();
+      introMusic.play();
+} } }
 
 export default addPoke;
